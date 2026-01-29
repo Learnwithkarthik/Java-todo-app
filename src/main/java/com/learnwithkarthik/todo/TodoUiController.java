8a688bd75456e55996e4f4965a4d621b31e9e0b0
laborant <laborant@labs.iximiuz.com>
2026-01-29 10:34:12 +0000

    added ui realted files and controllers

---
+++ src/main/java/com/learnwithkarthik/todo/TodoUiController.java
@@ -0,0 +1,32 @@
+package com.learnwithkarthik.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Controller
+public class TodoUiController {
+
+    private final TodoController todoController;
+
+    public TodoUiController(TodoController todoController) {
+        this.todoController = todoController;
+    }
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        Collection<Todo> todos = todoController.getTodos();
+        model.addAttribute("todos", todos);
+        return "index";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(@RequestParam String task) {
+        todoController.addTodo(Map.of("task", task));
+        return "redirect:/";
+    }
+}
+
